bfe2fe5782215fe73148c9efede423afb3d1903c
vivekgr92 <vivekgr92@gmail.com>
2022-01-26 10:32:28 -0800

    Add files to TMmodule

--- TMmodule/__main__.py
+++ TMmodule/__main__.py
@@ -1,75 +1,31 @@
 import sys, os, argparse, glob, pathlib, time
-print("Hello")
+import subprocess
+
+import numpy as np
+from natsort import natsorted
+from tqdm import tqdm
+import pdb
+from TMmodule import utils, models, io
+
+import logging
+logger = logging.getLogger(__name__)
 
 def main():
-    parser = argparse.ArgumentParser(description='cellpose parameters')
-    
-    # settings for CPU vs GPU
-    hardware_args = parser.add_argument_group("hardware arguments")
-    hardware_args.add_argument('--use_gpu', action='store_true', help='use gpu if torch or mxnet with cuda installed')
-    hardware_args.add_argument('--check_mkl', action='store_true', help='check if mkl working')
-    hardware_args.add_argument('--mkldnn', action='store_true', help='for mxnet, force MXNET_SUBGRAPH_BACKEND = "MKLDNN"')
-        
-    # settings for locating and formatting images
+    parser = argparse.ArgumentParser(description='Thinking Microscope parameters')
+
+
     input_img_args = parser.add_argument_group("input image arguments")
+    input_img_args.add_argument('--look_one_level_down', action='store_true', help='run processing on all subdirectories of current folder')
     input_img_args.add_argument('--dir',
                         default=[], type=str, help='folder containing data to run or train on.')
-    input_img_args.add_argument('--look_one_level_down', action='store_true', help='run processing on all subdirectories of current folder')
     input_img_args.add_argument('--mxnet', action='store_true', help='use mxnet')
-    input_img_args.add_argument('--img_filter',
-                        default=[], type=str, help='end string for images to run on')
-    input_img_args.add_argument('--channel_axis',
-                        default=None, type=int, help='axis of image which corresponds to image channels')
-    input_img_args.add_argument('--z_axis',
-                        default=None, type=int, help='axis of image which corresponds to Z dimension')
-    input_img_args.add_argument('--chan',
-                        default=0, type=int, help='channel to segment; 0: GRAY, 1: RED, 2: GREEN, 3: BLUE. Default: %(default)s')
-    input_img_args.add_argument('--chan2',
-                        default=0, type=int, help='nuclear channel (if cyto, optional); 0: NONE, 1: RED, 2: GREEN, 3: BLUE. Default: %(default)s')
-    input_img_args.add_argument('--invert', action='store_true', help='invert grayscale channel')
     input_img_args.add_argument('--all_channels', action='store_true', help='use all channels in image if using own model and images with special channels')
-    
+
     # model settings 
     model_args = parser.add_argument_group("model arguments")
     parser.add_argument('--pretrained_model', required=False, default='cyto', type=str, help='model to use')
     parser.add_argument('--unet', required=False, default=0, type=int, help='run standard unet instead of cellpose flow output')
     model_args.add_argument('--nclasses',default=3, type=int, help='if running unet, choose 2 or 3; if training omni, choose 4; standard Cellpose uses 3')
-
-
-    # algorithm settings
-    algorithm_args = parser.add_argument_group("algorithm arguments")
-    parser.add_argument('--omni', action='store_true', help='Omnipose algorithm (disabled by default)')
-    parser.add_argument('--cluster', action='store_true', help='DBSCAN clustering. Reduces oversegmentation of thin features (disabled by default).')
-    parser.add_argument('--fast_mode', action='store_true', help="make code run faster by turning off 4 network averaging")
-    parser.add_argument('--resample', action='store_true', help="run dynamics on full image (slower for images with large diameters)")
-    parser.add_argument('--no_interp', action='store_true', help='do not interpolate when running dynamics (was default)')
-    parser.add_argument('--do_3D', action='store_true', help='process images as 3D stacks of images (nplanes x nchan x Ly x Lx')
-    parser.add_argument('--diameter', required=False, default=30., type=float, 
-                        help='cell diameter, if 0 cellpose will estimate for each image')
-    parser.add_argument('--stitch_threshold', required=False, default=0.0, type=float, help='compute masks in 2D then stitch together masks with IoU>0.9 across planes')
-    
-    algorithm_args.add_argument('--flow_threshold', default=0.4, type=float, help='flow error threshold, 0 turns off this optional QC step. Default: %(default)s')
-    algorithm_args.add_argument('--mask_threshold', default=0, type=float, help='mask threshold, default is 0, decrease to find more and larger masks')
-    
-    parser.add_argument('--anisotropy', required=False, default=1.0, type=float,
-                        help='anisotropy of volume in 3D')
-    parser.add_argument('--diam_threshold', required=False, default=12.0, type=float, 
-                        help='cell diameter threshold for upscaling before mask rescontruction, default 12.')
-    parser.add_argument('--exclude_on_edges', action='store_true', help='discard masks which touch edges of image')
-    
-    # output settings
-    output_args = parser.add_argument_group("output arguments")
-    output_args.add_argument('--save_png', action='store_true', help='save masks as png and outlines as text file for ImageJ')
-    output_args.add_argument('--save_tif', action='store_true', help='save masks as tif and outlines as text file for ImageJ')
-    output_args.add_argument('--no_npy', action='store_true', help='suppress saving of npy')
-    output_args.add_argument('--savedir',
-                        default=None, type=str, help='folder to which segmentation results will be saved (defaults to input image directory)')
-    output_args.add_argument('--dir_above', action='store_true', help='save output folders adjacent to image folder instead of inside it (off by default)')
-    output_args.add_argument('--in_folders', action='store_true', help='flag to save output in folders (off by default)')
-    output_args.add_argument('--save_flows', action='store_true', help='whether or not to save RGB images of flows when masks are saved (disabled by default)')
-    output_args.add_argument('--save_outlines', action='store_true', help='whether or not to save RGB outline images when masks are saved (disabled by default)')
-    output_args.add_argument('--save_ncolor', action='store_true', help='whether or not to save minimal "n-color" masks (disabled by default')
-    output_args.add_argument('--save_txt', action='store_true', help='flag to enable txt outlines for ImageJ (disabled by default)')
 
     # training settings
     training_args = parser.add_argument_group("training arguments")
@@ -95,15 +51,79 @@ def main():
                         default=100, type=int, help='number of epochs to skip between saves. Default: %(default)s')
     training_args.add_argument('--save_each', action='store_true', help='save the model under a different filename per --save_every epoch for later comparsion')
     
-    # misc settings
-    parser.add_argument('--verbose', action='store_true', help='flag to output extra information (e.g. diameter metrics) for debugging and fine-tuning parameters')
-    parser.add_argument('--testing', action='store_true', help='flag to suppress CLI user confirmation for saving output; for test scripts')
-
-
+    
 
     args = parser.parse_args()
 
+    device, gpu = models.assign_device((not args.mxnet), True)
 
+    #define available model names, right now we have three broad categories 
+    model_names = ['cyto','nuclei','bact','cyto2','bact_omni','cyto2_omni']
+    builtin_model = np.any([args.pretrained_model==s for s in model_names])
+    cytoplasmic = 'cyto' in args.pretrained_model
+    szmean = 30.
+    rescale = True
+
+    # find images
+    img_filter = []
+    if len(img_filter)>0:
+        imf = img_filter
+    else:
+        imf = None
+
+    image_names = io.get_image_files(args.dir, 
+                                             args.mask_filter, 
+                                             imf=imf,
+                                             look_one_level_down=args.look_one_level_down)
+    nimg = len(image_names)
+
+
+    test_dir = None if len(args.test_dir)==0 else args.test_dir
+
+    output = io.load_train_test_data(args.dir, test_dir, imf, args.mask_filter, args.unet, args.look_one_level_down)
+    images, labels, image_names, test_images, test_labels, image_names_test = output
+
+    # training with all channels
+    if args.all_channels:
+        img = images[0]
+        if img.ndim==3:
+            nchan = min(img.shape)
+        elif img.ndim==2:
+            nchan = 1
+        channels = None 
+    else:
+        nchan = 2 
+
+    cpmodel_path = args.pretrained_model
+
+    model = models.CellposeModel(device=device,
+                                            torch=(not args.mxnet),
+                                            pretrained_model=cpmodel_path, 
+                                            diam_mean=szmean,
+                                            residual_on=args.residual_on,
+                                            style_on=args.style_on,
+                                            concatenation=args.concatenation,
+                                            nclasses=args.nclasses,
+                                            nchan=nchan,
+                                            omni=args.omni)
+
+    
+    
+    
+
+    # Train Segmentation Model
+    cpmodel_path = model.train(images, labels, train_files=image_names,
+                                           test_data=test_images, test_labels=test_labels, test_files=image_names_test,
+                                           learning_rate=args.learning_rate, channels=channels,
+                                           save_path=os.path.realpath(args.dir), save_every=args.save_every,
+                                           save_each=args.save_each,
+                                           rescale=rescale,n_epochs=args.n_epochs,
+                                           batch_size=args.batch_size, omni=False)
+
+   
+    logger.info('>>>> model trained and saved to %s'%cpmodel_path)
+
+    
 
 
 
